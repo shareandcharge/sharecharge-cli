7c6e2dff1170e47adbd3f420f56f497f277b73ac
adamstaveley <adam@motionwerk.com>
2018-06-19 18:02:54 +0200

    add other wallet commands

--- src/commands/wallet.logic.ts
+++ src/commands/wallet.logic.ts
@@ -3,23 +3,32 @@ import LogicBase from '../logicBase';
 import chalk from "chalk";
 import Inquirer from "../services/inquirer";
 const prompter = new Inquirer();
+const Web3 = require('web3');
 
 export default class WalletLogic extends LogicBase {
 
     public create = () => {
-        console.log('creating wallet...');
+        const newWallet = Wallet.generate();
+        console.log('Wallet created. Update the seed in your configuration to use.');
+        console.log(`coinbase: ${newWallet.wallet.coinbase}`);
+        console.log(`seed:     ${chalk.blue(newWallet.seed)}`);
     } 
 
     public info = () => {
         console.log('coinbase:', this.core.wallet.coinbase);
     }
 
-    public balance = () => {
-        console.log('getting balance...');
+    public balance = async () => {
+        const web3 = new Web3(this.core.config.ethProvider);
+        const balance = await web3.eth.getBalance(this.core.wallet.coinbase);
+        console.log('balance:', balance);
     }
 
-    public fund = () => {
-        console.log('requesting funds...');
+    public fund = async () => {
+        const web3 = new Web3(this.core.config.ethProvider);
+        const coinbase = await web3.eth.getCoinbase();
+        await web3.eth.sendTransaction({ from: coinbase, to: this.core.wallet.coinbase, value: web3.utils.toWei('1') });
+        console.log('funded wallet 1 ether');
     }
 
 }
