9d7df8a96d8fd3e8828dd2b6f566ffebe767e1fa
stipa <stipa@motionwerk.com>
2018-06-20 17:35:26 +0200

    fixed some minor things

--- src/commands/cdr.logic.ts
+++ src/commands/cdr.logic.ts
@@ -12,41 +12,37 @@ export default class CdrLogic extends LogicBase {
         }
     };
 
-    public filter = async (argv) => {
+    public getFilter = async (argv) => {
         const cdrInfo = await this.filterBy();
         console.log(JSON.stringify(cdrInfo, null, 2));
     }
 
     async getCDRInfo(argv): Promise<any> {
-        const filter = {};
-
-        const logDetails = await this.core.sc.charging.contract.getLogs('ChargeDetailRecord', filter);
-        let allLogs: any = logDetails.map(obj => (
-            {
-                timestamp: obj.returnValues.timestamp,
-                date: new Date(obj.returnValues.timestamp * 1000).toUTCString(),
-                evseId: ToolKit.hexToString(obj.returnValues.evseId),
-                scId: obj.returnValues.scId,
-                controller: obj.returnValues.controller,
-                finalPrice: obj.returnValues.finalPrice,
-                tokenAddress: obj.returnValues.tokenAddress,
-                chargingContract: obj.address,
-                transactionHash: obj.transactionHash,
-            }
-        ));
-
-        return allLogs;
-    }
-
-    async filterBy() {
-        const choice = (await prompter.filterBy()).filter;
-        const filter = {};
-
-        for(let element of choice) {
-            filter[element] = (await prompter.getInput(element)).filter;
+        
+        const filters = {};
+        // filtering
+        if (argv.scId) {
+            filters['scId'] = argv.scId.toLowerCase();
         }
 
-        const logDetails = await this.core.sc.charging.contract.getLogs('ChargeDetailRecord', filter);
+        if (argv.evseId) {
+            const evseHex = ToolKit.asciiToHex(argv.evseId);
+            const evsePadded = evseHex.padEnd(66, '0');
+            filters['evseId'] = evsePadded;
+        }
+        if (argv.controller) {
+            filters['controller'] = argv.controller.toLowerCase();
+        }
+        if (argv.tokenAddress) {
+            filters['tokenAddress'] = argv.tokenAddress.toLowerCase();
+        }
+        if (argv.start || argv.end) {
+            const start = Math.round(new Date(argv.start).getTime() / 1000 || 0);
+            const end = Math.round(new Date(argv.end).getTime() / 1000 || Date.now() / 1000);
+            filters['timestamp'] = { start, end };
+        }
+
+        const logDetails = await this.core.sc.charging.contract.getLogs('ChargeDetailRecord', filters);
         let allLogs: any = logDetails.map(obj => (
             {
                 timestamp: obj.returnValues.timestamp,
@@ -60,9 +56,41 @@ export default class CdrLogic extends LogicBase {
                 transactionHash: obj.transactionHash,
             }
         ));
-        
+
         return allLogs;
     }
 
+    async filterBy(): Promise<any> {
+        const filters = {};
 
+        const choice = (await prompter.filterBy()).filter;
+
+        for(let element of choice) {
+            if (element === 'evseId') {
+                let evse = (await prompter.getInput(element)).filter;
+                const evseHex = ToolKit.asciiToHex(evse);
+                const evsePadded = evseHex.padEnd(66, '0');
+                filters[element] = evsePadded;
+            }else {
+                filters[element] = (await prompter.getInput(element)).filter.toLowerCase();
+            }
+        }
+
+        const logDetails = await this.core.sc.charging.contract.getLogs('ChargeDetailRecord', filters);
+        let allLogs: any = logDetails.map(obj => (
+            {
+                timestamp: obj.returnValues.timestamp,
+                date: new Date(obj.returnValues.timestamp * 1000).toUTCString(),
+                evseId: ToolKit.hexToString(obj.returnValues.evseId),
+                scId: obj.returnValues.scId,
+                controller: obj.returnValues.controller,
+                finalPrice: obj.returnValues.finalPrice,
+                tokenAddress: obj.returnValues.tokenAddress,
+                chargingContract: obj.address,
+                transactionHash: obj.transactionHash,
+            }
+        ));
+
+        return allLogs;
+    }
 }
